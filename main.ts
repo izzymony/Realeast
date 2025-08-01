81d0f5c0e168f505967ef7f3ebea7c406602c848
izzymony <ojehonmonisrael008@gmail.com>
2025-08-01 13:45:45 +0100

    mastry

--- main.ts
+++ main.ts
@@ -1,9 +1,10 @@
 import postgres from 'postgres';
 import { drizzle } from 'drizzle-orm/postgres-js';
 import { PropertiesTable } from './src/drizzle/schema';
-import { v4 as uuidv4 } from 'uuid';
+
 
 async function main() {
+   
   const sql = postgres(process.env.DATABASE_URL!, {
     max: 1,
     idle_timeout: 20,
